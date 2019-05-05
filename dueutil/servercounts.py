20cbc00bc763104f7cee63b341ea815bd508b4b4
MatthewMinghetti <matthew_quebec@outlook.fr>
2019-05-05 18:59:58 -0400

    Pushing server count to https://discord.bots.gg/bots/464601463440801792

--- dueutil/servercounts.py
+++ dueutil/servercounts.py
@@ -9,13 +9,16 @@ CARBON_BOT_DATA = "https://www.carbonitex.net/discord/data/botdata.php"
 
 DISCORD_LIST = "https://bots.ondiscord.xyz/bot-api/bots/464601463440801792/guilds"
 BOTS_ORG = "https://discordbots.org/api/bots/464601463440801792/stats"
+BOTS_GG = "https://discord.bots.gg/api/v1/bots/464601463440801792/stats"
 
 
 async def update_server_count(shard):
 
-    await _carbon_server(shard)
+    # await _carbon_server(shard)
     await _post_shard_count_bod(shard, DISCORD_LIST, config["discordBotsKey"])
     await _post_shard_count_dbl(shard, BOTS_ORG, config["discordBotsOrgKey"])
+    await _post_shard_count_dbl(shard, BOTS_GG, config["discordBotsGGKey"])
+
 
 
 async def _carbon_server(shard):
@@ -25,6 +28,18 @@ async def _carbon_server(shard):
     carbon_payload = {"key": config["carbonKey"], "servercount": total_server_count}
     async with shard.session.post(CARBON_BOT_DATA, data=json.dumps(carbon_payload), headers=headers) as response:
         util.logger.info("Carbon returned %s status for the payload %s" % (response.status, carbon_payload))
+
+
+async def _post_shard_count_dbgg(shard, site, key):
+    # Seems like there is some form of standard?
+
+    headers = {"content-type": "application/json",
+               'authorization': key}
+    payload = {"server_count": len(shard.servers),
+               "shard_id": shard.shard_id,
+               "shard_count": len(util.shard_clients)}
+    async with shard.session.post(site, data=json.dumps(payload), headers=headers) as response:
+        util.logger.info(site+" returned %s for the payload %s" % (response.status, payload))
 
 
 async def _post_shard_count_bod(shard, site, key):
