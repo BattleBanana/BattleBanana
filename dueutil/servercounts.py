8cb65ddd6a51942d017337455cc16c40c1e51fe6
MatthewMinghetti <matthew_quebec@outlook.fr>
2019-06-16 11:05:44 -0400

    fixed dbl server count post

--- dueutil/servercounts.py
+++ dueutil/servercounts.py
@@ -59,7 +59,7 @@ async def _post_shard_count_dbl(shard, site, key):
 
     headers = {"content-type": "application/json",
                'authorization': key}
-    payload = {"server_count": shard.server_count,
+    payload = {"server_count": len(shard.servers),
                "shard_id": shard.shard_id,
                "shard_count": len(util.shard_clients)}
     async with shard.session.post(site, data=json.dumps(payload), headers=headers) as response:
