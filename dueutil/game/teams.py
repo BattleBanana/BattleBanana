25201d920dd41496186973563b912d83575f1464
DeveloperAnonymous <matthew_quebec@outlook.fr>
2019-09-25 16:52:33 -0400

    Modified team Class

--- dueutil/game/teams.py
+++ dueutil/game/teams.py
@@ -22,6 +22,29 @@ from .customizations import Theme
 from . import emojis as e
 
 
+class Teams(dict):
+    # Amount of time before the bot will prune a player.
+    PRUNE_INACTIVITY_TIME = 600  # (10 mins)
+
+    def prune(self):
+
+        """
+        Removes player that the bot has not seen 
+        for over an hour. If anyone mentions these
+        players (in a command) their data will be
+        fetched directly from the database
+        """
+        teams_pruned = 0
+        for id, team in list(self.items()):
+            del self[id]
+            teams_pruned += 1
+        gc.collect()
+        util.logger.info("Pruned %d teams for inactivity (10 minutes)", teams_pruned)
+
+
+teams = Teams()
+
+
 class Team(DueUtilObject, SlotPickleMixin):
     """
     Class about teams.. Update to make it save
@@ -30,18 +53,7 @@ class Team(DueUtilObject, SlotPickleMixin):
     __slots__ = ["name", "description", "level", "open", 
                 "owner", "admins", "members", "pendings", "id"]
 
-    def __init__(self, owner, name, description, level, isOpen, ctx):
-        if len(name) > 32 or len(name) < 4:
-            raise util.DueUtilException(ctx.channel, "Team Name must be between 4 and 32 characters")
-        if name != util.filter_string(name):
-            raise util.DueUtilException(ctx.channel, "Invalid team name!")
-        if name in customizations.teams:
-            raise util.DueUtilException(ctx.channel, "That team already exists!")
-        if level < 1:
-            raise util.DueUtilException(ctx.channel, "Minimum level cannot be under 1!")
-        if owner.team != None:
-            raise util.DueUtilException(ctx.channel, "You are already in a team!")
-
+    def __init__(self, owner, name, description, level, isOpen, **kwargs):
         self.name = name
         self.id = name.lower()
         self.description = description
@@ -51,39 +63,75 @@ class Team(DueUtilObject, SlotPickleMixin):
         self.admins = [owner]
         self.members = [owner]
         self.pendings = []
-        self.no_save = False
+        self.no_save = kwargs.pop("no_save") if "no_save" in kwargs else False
         self.save()
         owner.team = self
         owner.save()
-
+        
+    
     @property
-    def members(self):
-        return self.members
+    def avgLevel(self):
+        for member in self.members:
+            yield sum(member.level)/len(self.members) 
     
     def AddMember(self, ctx, member):
-        if member.id in self.members:
+        if member in self.members:
             raise util.DueUtilException(ctx.channel, "Already a member!")
+        member.team_invites.remove(self)
         self.members.append(member)
+        self.save()
+        member.save()
 
-    def AddAdmin(self, ctx, member):
-        if member.id in self.admins:
-            raise util.DueUtilException(ctx.channel, "Already an admin!")
-        self.admins.append(member)
-
-    def AddPending(self, ctx, member):
-        if member.id in self.pendings:
-            raise util.DueUtilException(ctx.channel, "Already pending!")
-        self.pendings.append(member)
-    
-    def RemoveMember(self, member):
-        if member.id in self.members:
+    def Kick(self, ctx, member):
+        if not (member in self.members):
+            raise util.DueUtilException(ctx.channel, "This player is not in the team")
+        if member in self.members:
             self.members.remove(member)
-        if member.id in self.admins:
+        if member in self.admins:
             self.admins.remove(member)
         member.Team = None
+        self.save()
+        member.save()
+
+    def AddAdmin(self, ctx, member):
+        if member in self.admins:
+            raise util.DueUtilException(ctx.channel, "Already an admin!")
+        self.admins.append(member)
+        member.save()
+        self.save()
+        
+    def RemoveAdmin(self, ctx, member):
+        if member not in self.admins:
+            raise util.DueUtilException(ctx.channel, "Not an admin!")
+        self.admins.remove(member)
+        self.save()
+        member.save()
+
+    def AddPending(self, ctx, member):
+        if member in self.pendings:
+            raise util.DueUtilException(ctx.channel, "Already pending!")
+        self.pendings.append(member)
+        self.save()
 
     def Delete(self):
         for member in self.members:
-            member.Team = None
+            self.members.remove(member)
+            member.team = None
             member.save()
         dbconn.get_collection_for_object(Team).remove({'_id': self.id})
+        
+def find_team(team_id: str) -> Team:
+    if team_id in teams:
+        return teams[team_id]
+    elif load_team(team_id):
+        return teams[team_id]
+
+REFERENCE_TEAM = Team(players.REFERENCE_PLAYER, "reference team", "Okay!", 1, False, no_save=True)
+
+def load_team(team_id):
+    response = dbconn.get_collection_for_object(Team).find_one({"_id": team_id})
+    if response is not None and 'data' in response:
+        team_data = response['data']
+        loaded_team = jsonpickle.decode(player_data)
+        teams[loaded_team.id] = util.load_and_update(REFERENCE_TEAM, loaded_team)
+        return True
