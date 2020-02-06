349207b694d182fe04904d33eeb5520434f27c75
Firescoutt <43297831+Firescoutt@users.noreply.github.com>
2020-02-06 18:53:02 +0000

    Russian Roulette when
    (Just the basic things until later)

--- dueutil/botcommands/gamble.py
+++ dueutil/botcommands/gamble.py
@@ -138,3 +138,26 @@ async def blackjack(ctx, price, **details):
     user.save()
     
     await util.edit_message(msg, embed=blackjack_embed)
+
+commands.command(args_pattern="I", aliases=["rr"])
+@commands.ratelimit(cooldown=30, error="You can't use Russian Roulette again for **[COOLDOWN]**!", save=True)
+async def russianroulette(ctx, price, **details):
+    """
+    [CMD_KEY]rusaianroulette ~~(bet)~~
+    
+    Play Russian Roulette with your friends.
+    
+    Game objective: Survive.
+    """
+
+# i do it when I figure it out 
+# aka googleing it and testing it with other bot
+
+    user = details["author"]
+    
+    if user.money < price:
+        raise util.DueUtilException(ctx.channel, "You cannot bet that much!")
+    if price < 1:
+        raise util.DueUtilException(ctx.channel, "You cannot bet under ¤1")
+    if (user.gamble_play and int(time.time() - user.last_played) < 120) or int(time.time() - user.last_played) < 120:
+        raise util.DueUtilException(ctx.channel, "You are already playing!")
