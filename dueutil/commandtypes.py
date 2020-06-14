d888a10d357b1b0d43cd48cbab43cc4c55c337fd
DeveloperAnonymous <matthew_quebec@outlook.fr>
2020-06-14 17:29:10 -0400

    Fixed player's parsing

--- dueutil/commandtypes.py
+++ dueutil/commandtypes.py
@@ -70,7 +70,7 @@ def parse_float(value):
 
 def parse_player(player_id, called, ctx):
     # A DueUtil Player
-    player = players.find_player(player_id)
+    player = players.find_player(int(player_id))
     if player is None or not player.is_playing(ctx.guild) \
             and called.permission < Permission.DUEUTIL_MOD:
         return False
