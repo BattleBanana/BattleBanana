0951e6ffcc95e418f8375399ddd6686121c25241
DeveloperAnonymous <matthew_quebec@outlook.fr>
2021-04-25 22:22:50 -0400

    Modified resizing algorithm (yes again xD)

--- dueutil/game/helpers/imagehelper.py
+++ dueutil/game/helpers/imagehelper.py
@@ -172,7 +172,7 @@ async def load_image_url(url, **kwargs):
 def resize(image, width, height):
     if image is None:
         return None
-    return image.resize((width, height), Image.ANTIALIAS)
+    return image.resize((width, height), Image.BILINEAR)
 
 
 async def resize_avatar(player, server, width, height):
