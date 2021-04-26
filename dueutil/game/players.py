be49754112322db2a83f678b74daf2bed27100cd
DeveloperAnonymous <matthew_quebec@outlook.fr>
2021-04-25 22:22:27 -0400

    Optimized player data transfer

--- dueutil/game/players.py
+++ dueutil/game/players.py
@@ -497,10 +497,11 @@ async def handle_client(reader, writer):
     error_found = False
     try:
         request = json.loads(request)
-        player = find_player(int(request['id'])) or find_player(str(request['id']))
+        id = int(request['id'])
+        player = find_player(id)
         if player is None: # no account on BattleBanana
-            Player(FakeMember(int(request['id'])))
-            player = await find_player(int(request['id']))
+            Player(FakeMember(id))
+            player = await find_player(id)
     except json.decoder.JSONDecodeError:
         player = None
         error_found = True
