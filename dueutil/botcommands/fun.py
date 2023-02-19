d89a5e1734703c6d20905ddf8086dc1ff910eb7e
zen0-5338 <shahjeet767@gmail.com>
2023-02-19 16:02:26 +0530

    changed tdh to display time according to local tz

--- dueutil/botcommands/fun.py
+++ dueutil/botcommands/fun.py
@@ -430,7 +430,7 @@ async def topdoghistory(ctx, page=1, **_):
         TOPDOGS_PER_PAGE)
 
     embed = discord.Embed(title="Topdog History", type="rich", color=gconf.DUE_COLOUR)
-    embed.set_footer(text="Times are in UTC.")
+    embed.set_footer(text="Times are displayed according to your timezone.")
 
     topdog = awards.get_award_stat("TopDog")
     if topdog is None or "top_dog" not in topdog:
@@ -446,11 +446,11 @@ async def topdoghistory(ctx, page=1, **_):
             date: datetime = topdog.get('date')
 
             if util.is_today(date):
-                tdstring += f"- **{player.name}**, today at {date.strftime('%H:%M')}\n"
+                tdstring += f"- **{player.name}**, today until <t:{round(date.timestamp())}:t>\n"
             elif util.is_yesterday(date):
-                tdstring += f"- **{player.name}**, yesterday at {date.strftime('%H:%M')}\n"
+                tdstring += f"- **{player.name}**, yesterday until <t:{round(date.timestamp())}:t>\n"
             else:
-                tdstring += f"- **{player.name}**, at {date.strftime('%d/%m/%Y')}\n"
+                tdstring += f"- **{player.name}**, at <t:{round(date.timestamp())}:D>\n"
 
     embed.add_field(name="Previous topdogs:", value=tdstring or "No previous topdogs", inline=False)
 
