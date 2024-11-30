cf236c27a056fda957be0dd4ec5516a03e68fe78
DeveloperAnonymous <40847862+DeveloperAnonymous@users.noreply.github.com>
2024-11-30 01:35:02 -0500

    Readded clamping to number arguments parsing

--- dueutil/commandtypes.py
+++ dueutil/commandtypes.py
@@ -7,6 +7,8 @@ from .game.helpers import misc
 from .permissions import Permission
 
 # The max number the bot will accept. To avoid issues with crazy big numbers.
+MAX_NUMBER = sys.maxsize
+MIN_NUMBER = -MAX_NUMBER
 STRING_TYPES = ("S", "M")
 THOUSANDS_REGEX = re.compile(r"(\,)(\d\d\d)")
 
@@ -40,7 +42,7 @@ def parse_team(value):
 def parse_int(value):
     # An int limited between min and max number
     try:
-        return int(strip_thousands_separators(value))
+        return util.clamp(int(strip_thousands_separators(value)), MIN_NUMBER, MAX_NUMBER)
     except ValueError:
         return False
 
@@ -75,7 +77,7 @@ def parse_count(value):
 def parse_float(value):
     # Float between min and max number
     try:
-        return float(strip_thousands_separators(value))
+        return util.clamp(float(strip_thousands_separators(value)), MIN_NUMBER, MAX_NUMBER)
     except ValueError:
         return False
 
