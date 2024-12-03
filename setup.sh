5be840edc564aced4a22d7ecad46ac5c82636a73
DeveloperAnonymous <40847862+DeveloperAnonymous@users.noreply.github.com>
2024-12-02 22:41:40 -0500

    Make the setup use docker, containerization >>>>>> os

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
-# Install python
-sudo apt-get install python3 -y
-sudo apt-get install python3-pip -y
-python3 -c 'import sys; assert sys.version_info >= (3,13), "Python 3.13 or higher required"'
-
-# Install MongoDB
-sudo apt-get install gnupg curl
-curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
-   --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Install requirements
-sudo apt-get install --no-install-recommends -y build-essential libfuzzy-dev libwebp-dev
-python3 -m pip install -r requirements.txt
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
