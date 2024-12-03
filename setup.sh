fede925ec685cfb1c02e85bd01b05098da0622b3
DeveloperAnonymous <40847862+DeveloperAnonymous@users.noreply.github.com>
2024-12-02 21:38:24 -0500

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
+# Install python
 sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
 python3 -c 'import sys; assert sys.version_info >= (3,13), "Python 3.13 or higher required"'
+
+# Install MongoDB
 sudo apt-get install gnupg curl
 curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
@@ -8,3 +11,7 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
+# Install requirements
+sudo apt-get install --no-install-recommends -y build-essential libfuzzy-dev libwebp-dev
+python3 -m pip install -r requirements.txt
