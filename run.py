c86ee652a8a9046649d88ae9b416a9038b4dd395
DeveloperAnonymous <40847862+DeveloperAnonymous@users.noreply.github.com>
2025-05-01 00:02:33 -0400

    forgot on shard ready changes

--- run.py
+++ run.py
@@ -401,11 +401,9 @@ class BattleBananaClient(discord.AutoShardedClient):
             util.logger.error("Failed to change presence")
 
         util.logger.info(
-            "\nLogged in shard %d as %s\nWith account @%s ID:%s \n-------",
+            "Shard %d (%s) ready",
             shard_id + 1,
             shard_names[shard_id],
-            self.user.name,
-            self.user.id,
         )
 
 
